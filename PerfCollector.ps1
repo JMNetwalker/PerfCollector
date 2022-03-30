04387dd133a3e1e6b6f91452333d1f5193c502aa
Jose Manuel Jurado Diaz <jmjurado@microsoft.com>
2022-03-30 22:02:04 +0200

    Add files via upload
    PowerShell Scripts

---
+++ PerfCollector.ps1
@@ -0,0 +1,311 @@
+﻿#----------------------------------------------------------------
+# Application: Performance Checker
+# Propose: Inform about performance 
+#----------------------------------------------------------------
+
+#----------------------------------------------------------------
+#Parameters 
+#----------------------------------------------------------------
+param($server = "", #ServerName parameter to connect 
+      $user = "", #UserName parameter  to connect
+      $passwordSecure = "", #Password Parameter  to connect
+      $Db = "", #DBName Parameter  to connect
+      $Folder = "C:\PerfChecker") #Folder Paramater to save the csv files 
+
+
+
+function CheckStatistics($connection)
+{
+ try
+ {
+   logMsg( "---- Checking Statistics health (Started) ---- " ) (1)
+   $command = New-Object -TypeName System.Data.SqlClient.SqlCommand
+   $command.CommandTimeout = 60
+   $command.Connection=$connection
+   $command.CommandText = "SELECT sp.stats_id, stat.name, o.name, filter_definition, last_updated, rows, rows_sampled, steps, unfiltered_rows, modification_counter,  DATEDIFF(DAY, last_updated , getdate()) AS Diff   
+                           FROM sys.stats AS stat   
+                           Inner join sys.objects o on stat.object_id=o.object_id
+                           CROSS APPLY sys.dm_db_stats_properties(stat.object_id, stat.stats_id) AS sp  
+                           WHERE o.type = 'U' order by o.name, stat.name"
+  $Reader = $command.ExecuteReader(); #Executing the Recordset
+  while($Reader.Read())
+   {
+     if( $Reader.GetValue(5) -gt $Reader.GetValue(6))
+     {
+       logMsg("Table: " + $Reader.GetValue(2).ToString() + "/" + $Reader.GetValue(1).ToString() + " possible AUTO_STATS executed" ) (2)
+     }
+     if( TestEmpty($Reader.GetValue(10))) {}
+     else
+     {
+      if($Reader.GetValue(10) -gt 15)
+      {
+       logMsg("Table: " + $Reader.GetValue(2).ToString() + "/" + $Reader.GetValue(1).ToString() + " possible outdated" ) (2)
+      }
+     }
+   }
+
+   $Reader.Close();
+   logMsg( "---- Checking Statistics health (Finished) ---- " ) (1)
+  }
+  catch
+   {
+    logMsg("Not able to run statistics health checker..." + $Error[0].Exception) (2)
+   } 
+
+}
+
+
+
+#----------------------------------------------------------------
+#Function to connect to the database using a retry-logic
+#----------------------------------------------------------------
+
+Function GiveMeConnectionSource()
+{ 
+  for ($i=1; $i -lt 10; $i++)
+  {
+   try
+    {
+      logMsg( "Connecting to the database...Attempt #" + $i) (1)
+      $SQLConnection = New-Object System.Data.SqlClient.SqlConnection 
+      $SQLConnection.ConnectionString = "Server="+$server+";Database="+$Db+";User ID="+$user+";Password="+$password+";Connection Timeout=60" 
+      $SQLConnection.Open()
+      logMsg("Connected to the database...") (1)
+      return $SQLConnection
+      break;
+    }
+  catch
+   {
+    logMsg("Not able to connect - Retrying the connection..." + $Error[0].Exception) (2)
+    Start-Sleep -s 5
+   }
+  }
+}
+
+#--------------------------------------------------------------
+#Create a folder 
+#--------------------------------------------------------------
+Function CreateFolder
+{ 
+  Param( [Parameter(Mandatory)]$Folder ) 
+  try
+   {
+    $FileExists = Test-Path $Folder
+    if($FileExists -eq $False)
+    {
+     $result = New-Item $Folder -type directory 
+     if($result -eq $null)
+     {
+      logMsg("Imposible to create the folder " + $Folder) (2)
+      return $false
+     }
+    }
+    return $true
+   }
+  catch
+  {
+   return $false
+  }
+ }
+
+#-------------------------------
+#Create a folder 
+#-------------------------------
+Function DeleteFile{ 
+  Param( [Parameter(Mandatory)]$FileName ) 
+  try
+   {
+    $FileExists = Test-Path $FileNAme
+    if($FileExists -eq $True)
+    {
+     Remove-Item -Path $FileName -Force 
+    }
+    return $true 
+   }
+  catch
+  {
+   return $false
+  }
+ }
+
+#--------------------------------
+#Log the operations
+#--------------------------------
+function logMsg
+{
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $msg,
+         [Parameter(Mandatory=$false, Position=1)]
+         [int] $Color
+    )
+  try
+   {
+    $Fecha = Get-Date -format "yyyy-MM-dd HH:mm:ss"
+    $msg = $Fecha + " " + $msg
+    Write-Output $msg | Out-File -FilePath $LogFile -Append
+    $Colores="White"
+    $BackGround = 
+    If($Color -eq 1 )
+     {
+      $Colores ="Cyan"
+     }
+    If($Color -eq 3 )
+     {
+      $Colores ="Yellow"
+     }
+
+     if($Color -eq 2)
+      {
+        Write-Host -ForegroundColor White -BackgroundColor Red $msg 
+      } 
+     else 
+      {
+        Write-Host -ForegroundColor $Colores $msg 
+      } 
+
+
+   }
+  catch
+  {
+    Write-Host $msg 
+  }
+}
+
+#--------------------------------
+#The Folder Include "\" or not???
+#--------------------------------
+
+function GiveMeFolderName([Parameter(Mandatory)]$FolderSalida)
+{
+  try
+   {
+    $Pos = $FolderSalida.Substring($FolderSalida.Length-1,1)
+    If( $Pos -ne "\" )
+     {return $FolderSalida + "\"}
+    else
+     {return $FolderSalida}
+   }
+  catch
+  {
+    return $FolderSalida
+  }
+}
+
+#--------------------------------
+#Validate Param
+#--------------------------------
+function TestEmpty($s)
+{
+if ([string]::IsNullOrWhitespace($s))
+  {
+    return $true;
+  }
+else
+  {
+    return $false;
+  }
+}
+
+#--------------------------------
+#Separator
+#--------------------------------
+
+function GiveMeSeparator
+{
+Param([Parameter(Mandatory=$true)]
+      [System.String]$Text,
+      [Parameter(Mandatory=$true)]
+      [System.String]$Separator)
+  try
+   {
+    [hashtable]$return=@{}
+    $Pos = $Text.IndexOf($Separator)
+    $return.Text= $Text.substring(0, $Pos) 
+    $return.Remaining = $Text.substring( $Pos+1 ) 
+    return $Return
+   }
+  catch
+  {
+    $return.Text= $Text
+    $return.Remaining = ""
+    return $Return
+  }
+}
+
+try
+{
+Clear
+
+#--------------------------------
+#Check the parameters.
+#--------------------------------
+
+if (TestEmpty($server)) { $server = read-host -Prompt "Please enter a Server Name" }
+if (TestEmpty($user))  { $user = read-host -Prompt "Please enter a User Name"   }
+if (TestEmpty($passwordSecure))  
+    {  
+    $passwordSecure = read-host -Prompt "Please enter a password"  -assecurestring  
+    $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwordSecure))
+    }
+else
+    {$password = $passwordSecure} 
+if (TestEmpty($Db))  { $Db = read-host -Prompt "Please enter a Database Name"  }
+if (TestEmpty($Folder)) {  $Folder = read-host -Prompt "Please enter a Destination Folder (Don't include the past \) - Example c:\QdsExport" }
+
+Function Remove-InvalidFileNameChars {
+
+param([Parameter(Mandatory=$true,
+    Position=0,
+    ValueFromPipeline=$true,
+    ValueFromPipelineByPropertyName=$true)]
+    [String]$Name
+)
+
+return [RegEx]::Replace($Name, "[{0}]" -f ([RegEx]::Escape([String][System.IO.Path]::GetInvalidFileNameChars())), '')}
+
+#--------------------------------
+#Run the process
+#--------------------------------
+
+
+logMsg("Creating the folder " + $Folder) (1)
+   $result = CreateFolder($Folder) #Creating the folder that we are going to have the results, log and zip.
+   If( $result -eq $false)
+    { 
+     logMsg("Was not possible to create the folder") (2)
+     exit;
+    }
+logMsg("Created the folder " + $Folder) (1)
+
+$sFolderV = GiveMeFolderName($Folder) #Creating a correct folder adding at the end \.
+
+$LogFile = $sFolderV + "PerfChecker.Log"    #Logging the operations.
+
+logMsg("Deleting Log") (1)
+   $result = DeleteFile($LogFile) #Delete Log file
+logMsg("Deleted Log") (1)
+
+   $SQLConnectionSource = GiveMeConnectionSource #Connecting to the database.
+   if($SQLConnectionSource -eq $null)
+    { 
+     logMsg("It is not possible to connect to the database") (2)
+     exit;
+    }
+
+ CheckStatistics( $SQLConnectionSource)
+
+ logMsg("Closing the connection..") (1)
+ $SQLConnectionSource.Close() 
+ Remove-Variable password
+ logMsg("Performance Collector Script was executed correctly")  (1)
+}
+catch
+  {
+    logMsg("Performance Collector Script was executed incorrectly ..: " + $Error[0].Exception) (2)
+  }
+finally
+{
+   logMsg("Performance Collector Script finished - Check the previous status line to know if it was success or not") (2)
+} 
